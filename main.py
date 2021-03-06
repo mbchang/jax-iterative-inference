2cc38515b00aee71f1135f9e2c365f2368c73350
Nalini Singh <nmsingh@mit.edu>
2021-03-06 11:36:33 -0500

    Comply with PEP 8

--- main.py
+++ main.py
@@ -23,13 +23,14 @@ latent_dim = 64
 encoder_init, encode = stax.serial(
     stax.parallel(
         stax.serial(Dense(512), Relu),  # image
-        stax.serial(stax.FanInConcat(), Dense(256), Relu)  # mu, logvar, gradmu, gradlogvar
+        # mu, logvar, gradmu, gradlogvar
+        stax.serial(stax.FanInConcat(), Dense(256), Relu)
     ),
     stax.FanInConcat(),
     Dense(512), Relu,
     FanOut(2),
-    stax.parallel(Dense(latent_dim), 
-        Dense(latent_dim)),
+    stax.parallel(Dense(latent_dim),
+                  Dense(latent_dim)),
 )
 
 decoder_init, decode = stax.serial(
@@ -37,6 +38,7 @@ decoder_init, decode = stax.serial(
     Dense(512), Relu,
     Dense(28 * 28),
 )
+
 
 def gaussian_kl(z_dist):
     """KL divergence from a diagonal Gaussian to the standard Gaussian."""
@@ -58,11 +60,13 @@ def bernoulli_logpdf(logits, x):
 def get_logits(rng, dec_params):
     return lambda z_dist: decode(dec_params, gaussian_sample(rng, z_dist))
 
+
 def inner_elbo_fcn(rng, dec_params):
     def get_elbo(z_dist, images):
         logits_x = get_logits(rng, dec_params)(z_dist)
         return bernoulli_logpdf(logits_x, images) - gaussian_kl(z_dist)
     return get_elbo
+
 
 def outer_loss(rng, params, images, num_steps, return_images=False):
     enc_params, dec_params, prior_mu_z, prior_logvar_z = params
@@ -70,12 +74,21 @@ def outer_loss(rng, params, images, num_steps, return_images=False):
     logvar_z_norms = []
     mu_z_norms = []
     elbos = []
-    if(return_images): 
+    if(return_images):
         logit_images = [images]
 
-
-    prior_mu_z = jnp.repeat(jnp.expand_dims(prior_mu_z, 0), repeats=images.shape[0], axis=0) # (B, D)
-    prior_logvar_z = jnp.repeat(jnp.expand_dims(prior_logvar_z, 0), repeats=images.shape[0], axis=0) # (B, D)
+    prior_mu_z = jnp.repeat(
+        jnp.expand_dims(
+            prior_mu_z,
+            0),
+        repeats=images.shape[0],
+        axis=0)  # (B, D)
+    prior_logvar_z = jnp.repeat(
+        jnp.expand_dims(
+            prior_logvar_z,
+            0),
+        repeats=images.shape[0],
+        axis=0)  # (B, D)
     z_dist = GaussianParams(mu=prior_mu_z, logvar=prior_logvar_z)
     loss = 0
     for t in range(num_steps):
@@ -85,26 +98,27 @@ def outer_loss(rng, params, images, num_steps, return_images=False):
 
         elbo, elbo_grad = vmapped_value_and_grad(z_dist, images)
 
-        posterior_mu_z, posterior_logvar_z = encode(enc_params,
-                                                    (images, (z_dist.mu, z_dist.logvar, elbo_grad.mu, elbo_grad.logvar)))
-        
+        posterior_mu_z, posterior_logvar_z = encode(
+            enc_params, (images, (z_dist.mu, z_dist.logvar, elbo_grad.mu, elbo_grad.logvar)))
 
         logvar_z_norm = jnp.linalg.norm(posterior_logvar_z[0])
         mu_z_norm = jnp.linalg.norm(posterior_mu_z[0])
 
         z_dist = GaussianParams(mu=posterior_mu_z, logvar=posterior_logvar_z)
 
-        if(return_images): 
+        if(return_images):
             logits_x = get_logits(rng, dec_params)(z_dist)
             logit_images.append(jax.nn.sigmoid(logits_x))
         logvar_z_norms.append(logvar_z_norm)
         mu_z_norms.append(mu_z_norm)
-        elbos.append(elbo[0])#.primal)
+        elbos.append(elbo[0])  # .primal)
 
         loss += jnp.sum(elbo)
 
-    to_return = (loss/num_steps, logit_images, logvar_z_norms, elbos) if return_images else loss/num_steps
+    to_return = (loss / num_steps, logit_images, logvar_z_norms,
+                 elbos) if return_images else loss / num_steps
     return to_return
+
 
 def main():
     config.update("jax_debug_nans", True)
@@ -128,9 +142,8 @@ def main():
     enc_init_rng, dec_init_rng, mu_init_rng = random.split(
         random.PRNGKey(0), 3)
 
-
-    _, init_encoder_params = encoder_init(
-        enc_init_rng, ((batch_size, 28*28), [(batch_size, latent_dim) for i in range(4)]))
+    _, init_encoder_params = encoder_init(enc_init_rng, ((
+        batch_size, 28 * 28), [(batch_size, latent_dim) for i in range(4)]))
 
     _, init_decoder_params = decoder_init(
         dec_init_rng, (batch_size, latent_dim))
@@ -145,10 +158,10 @@ def main():
     test_images = jax.device_put(test_images)
 
     def binarize_batch(rng, i, images):
-            i = i % num_batches
-            batch = lax.dynamic_slice_in_dim(
-                images, i * batch_size, batch_size)
-            return random.bernoulli(rng, batch)
+        i = i % num_batches
+        batch = lax.dynamic_slice_in_dim(
+            images, i * batch_size, batch_size)
+        return random.bernoulli(rng, batch)
 
     @jit
     def run_epoch(rng, opt_state, images):
@@ -157,7 +170,7 @@ def main():
             batch = binarize_batch(data_rng, i, images)
 
             def loss(params): return -outer_loss(elbo_rng,
-                                                params, batch, num_steps) / batch_size
+                                                 params, batch, num_steps) / batch_size
             g = grad(loss)(get_params(opt_state))
             return opt_update(i, g, opt_state)
         return lax.fori_loop(0, num_batches, body_fun, opt_state)
@@ -166,43 +179,50 @@ def main():
         params = get_params(opt_state)
         elbo_rng, data_rng, image_rng = random.split(test_rng, 3)
         num_test = 10
-        binarized_test = random.bernoulli(data_rng, images[:num_test,:])
-        test_elbo, logit_images, logvar_z_norms, elbos = outer_loss(elbo_rng, params,
-                                binarized_test, num_steps, True) 
+        binarized_test = random.bernoulli(data_rng, images[:num_test, :])
+        test_elbo, logit_images, logvar_z_norms, elbos = outer_loss(
+            elbo_rng, params, binarized_test, num_steps, True)
         stack_logit_images = jnp.stack(logit_images)
-        sampled_logit_images = image_grid(num_steps+1, num_test, stack_logit_images, (28, 28))
-        return -test_elbo/images.shape[0], sampled_logit_images, logvar_z_norms, elbos
+        sampled_logit_images = image_grid(
+            num_steps + 1, num_test, stack_logit_images, (28, 28))
+        return -test_elbo / \
+            images.shape[0], sampled_logit_images, logvar_z_norms, elbos
 
     def image_grid(nrow, ncol, imagevecs, imshape):
         """Reshape a stack of image vectors into an image grid for plotting."""
         images = iter(imagevecs.reshape((-1,) + imshape))
         return [[next(images).T for _ in range(ncol)][::-1]
-                        for _ in range(nrow)]
+                for _ in range(nrow)]
 
     opt_state = opt_init(init_params)
     for epoch in range(num_epochs):
         tic = time.time()
         if not debug:
-            opt_state = run_epoch(random.PRNGKey(epoch), opt_state, train_images)
-            test_elbo, logit_images, logvar_norms, elbos = jit(evaluate)(opt_state, test_images)
+            opt_state = run_epoch(
+                random.PRNGKey(epoch), opt_state, train_images)
+            test_elbo, logit_images, logvar_norms, elbos = jit(
+                evaluate)(opt_state, test_images)
         else:
             rng = random.PRNGKey(epoch)
             for i in range(num_batches):
                 elbo_rng, data_rng = random.split(random.fold_in(rng, i))
                 batch = binarize_batch(data_rng, i, train_images)
-                loss = lambda params: -outer_loss(elbo_rng, params, batch, num_steps) / batch_size
+
+                def loss(params): return -outer_loss(elbo_rng,
+                                                     params, batch, num_steps) / batch_size
 
                 g = grad(loss)(get_params(opt_state))
                 opt_state = opt_update(i, g, opt_state)
 
-            test_elbo, logit_images, logvar_norms, elbos = evaluate(opt_state, test_images)
+            test_elbo, logit_images, logvar_norms, elbos = evaluate(
+                opt_state, test_images)
 
         fig, axs = plt.subplots(
             len(logit_images[0]),
-            len(logit_images), 
-            figsize=(2*len(logit_images),2*len(logit_images[0])),
+            len(logit_images),
+            figsize=(2 * len(logit_images), 2 * len(logit_images[0])),
             constrained_layout=True)
-        fig.suptitle('Training Epoch: '+str(epoch), fontsize=36)
+        fig.suptitle('Training Epoch: ' + str(epoch), fontsize=36)
         for i in range(len(logit_images[0])):
             for j in range(len(logit_images)):
                 axs[i][j].imshow(logit_images[j][i].T, cmap=plt.cm.gray)
@@ -216,5 +236,6 @@ def main():
         plt.savefig(imfile.format(epoch))
         plt.close()
 
-if __name__=="__main__":        
+
+if __name__ == "__main__":
     main()
--- plot_gif.py
+++ plot_gif.py
@@ -2,11 +2,12 @@ import glob
 import imageio
 import os
 
+
 def save_gif(folder, delimiter):
     """
         expects images to be saved like this:
 
-        <folder> 
+        <folder>
             <delimiter>0.png
             <delimiter>1.png
             <delimiter>2.png
@@ -14,20 +15,28 @@ def save_gif(folder, delimiter):
     def get_key(fname):
         basename = os.path.basename(fname)
         start = basename.rfind(delimiter)
-        key = str(int(basename[start+len(delimiter):-len('.png')])).zfill(3)
+        key = str(int(basename[start + len(delimiter):-len('.png')])).zfill(3)
         return key
 
-    fnames = sorted(glob.glob('{}/{}*.png'.format(folder, delimiter)), key=get_key)
+    fnames = sorted(
+        glob.glob('{}/{}*.png'.format(folder, delimiter)), key=get_key)
     print(fnames)
-    
+
     if len(fnames) > 0:
         images = [imageio.imread(fname) for fname in fnames]
-        imageio.mimsave(os.path.join(folder, '{}.gif'.format(delimiter)), images, fps=2)
+        imageio.mimsave(
+            os.path.join(
+                folder,
+                '{}.gif'.format(delimiter)),
+            images,
+            fps=2)
     else:
         print('Could not find any images in {}'.format(folder))
 
-def main():
-    save_gif('vae_samples','mnist_vae_')
 
-if __name__=="__main__":        
+def main():
+    save_gif('vae_samples', 'mnist_vae_')
+
+
+if __name__ == "__main__":
     main()
